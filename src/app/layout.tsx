517b449c76d8dbd834169e1cb0c0f39e3a66aaf9
sirawit.mahanin <sirawit.mn@gmail.com>
2025-02-22 17:16:14 +0700

    Add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Dependabots smasher",
   description: "",
+  icons: {
+    icon: "/icon.png"
+  }
 };
 
 export default function RootLayout({
